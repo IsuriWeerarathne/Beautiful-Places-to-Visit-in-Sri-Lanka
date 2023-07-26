8d2943744a278eb74f5e1906366638d8caba23e5
IsuriWeerarathne <isuriweerarathna@gmail.com>
2023-07-26 23:52:17 +0530

    fifth

--- travelSL/src/Script.js
+++ travelSL/src/Script.js
@@ -1,1 +1,12 @@
-console.log("Hi");
+function validateForm() {
+    var x = document.forms["feedback"]["comment"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+    x=document.forms["feedback"]["email"].value;
+    if (x == "") {
+        alert("Email must be filled out");
+        return false;
+    }
+}
